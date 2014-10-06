504105301060b94744cdce1e11012bebf8fe989a
Marcin Maciukiewicz <mm@bluepocket.com>
2014-10-06 21:51:23 +0100

    View controllers added

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Marcin Maciukiewicz on 06/10/2014.
+//  Copyright (c) 2014 Marcin Maciukiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
